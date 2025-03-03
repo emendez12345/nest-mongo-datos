206e9d91dbe507ddb4a5098d5319b7a8d4af7052
eamv0066 <adesarrollo4@unico.com.co>
2025-03-03 11:58:54 -0500

    Correcion de appModules conexion con la data

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,21 +5,10 @@ import { ProductsController } from './controllers/products.controller';
 import { CategoriesController } from './controllers/categories.controller';
 import { ProductsService } from './services/products.service';
 import { ExportModule } from './export/export.module';
+import { DatabaseModule } from './database/database.module';
 
-// const uri = 'mongodb://root:root@localhost:27017/'
-
-// const client = new MongoClient(uri);
-
-// async function run() {
-//   await client.connect();
-//   const database = client.db('platzi-store');
-//   const taskColletion = database.collection('task');
-//   const tasks= await taskColletion.find().toArray();
-//   console.log(tasks)
-// }
-// run();
 @Module({
-  imports: [ExportModule],
+  imports: [ExportModule,DatabaseModule],
   controllers: [AppController, ProductsController, CategoriesController],
   providers: [AppService, ProductsService],
 })
--- src/export/export.controller.ts
+++ src/export/export.controller.ts
@@ -18,38 +18,3 @@ export class ExportController {
     await this.exportService.generateExcel(res);
   }
 }
-
-// import { Controller, Get, Post, Body, Put, Param, Delete } from '@nestjs/common';
-// import { ExportService } from './export.service';
-// import { CreateExportDto } from './dto/create-export.dto';
-// import { UpdateExportDto } from './dto/update-export.dto';
-
-// @Controller('export')
-// export class ExportController {
-//   constructor(private readonly exportService: ExportService) {}
-
-//   @Post()
-//   create(@Body() createExportDto: CreateExportDto) {
-//     return this.exportService.create(createExportDto);
-//   }
-
-//   @Get()
-//   findAll() {
-//     return this.exportService.findAll();
-//   }
-
-//   @Get(':id')
-//   findOne(@Param('id') id: string) {
-//     return this.exportService.findOne(+id);
-//   }
-
-//   @Put(':id')
-//   update(@Param('id') id: string, @Body() updateExportDto: UpdateExportDto) {
-//     return this.exportService.update(+id, updateExportDto);
-//   }
-
-//   @Delete(':id')
-//   remove(@Param('id') id: string) {
-//     return this.exportService.remove(+id);
-//   }
-// }
